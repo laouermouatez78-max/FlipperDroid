afb9c57da71df315bd81762b8ed9fc8d4acdb264
laouermouatez78-max <laouermouatez78@gmail.com>
2026-08-30 01:23:12 +0100

    Bump FlipperDroid to V5

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.flipperdroid"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0.0"
+        versionCode = 5
+        versionName = "5.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
