3a57790458c54d37ec7c8fc865c2b9f13f36abdb
laouermouatez78-max <laouermouatez78@gmail.com>
2026-08-30 01:23:02 +0100

    Wire V5 OSINT Hub into navigation

--- src/main/java/com/example/flipperdroid/Main.kt
+++ src/main/java/com/example/flipperdroid/Main.kt
@@ -29,6 +29,7 @@ class MainActivity : ComponentActivity() {
     private val networkToolsViewModel: NetworkToolsViewModel by viewModels()
     private val wifiAuditViewModel: WifiDeautherViewModel by viewModels()
     private val lanAnalyzerViewModel: LanAnalyzerViewModel by viewModels()
+    private val osintViewModel: OsintViewModel by viewModels()
     private val usbInspectorViewModel: UsbInspectorViewModel by viewModels()
     private val badUsbViewModel: BadUsbViewModel by viewModels()
     private val emvCardEmulationViewModel: EmvCardEmulationViewModel by viewModels()
@@ -118,6 +119,7 @@ class MainActivity : ComponentActivity() {
             composable("wifi_deauther") { WifiDeautherScreen(navController, wifiAuditViewModel) }
             composable("lan_analyzer") { LanAnalyzerScreen(navController, lanAnalyzerViewModel) }
             composable("network") { NetworkToolsScreen(navController, networkToolsViewModel) }
+            composable("osint") { OsintScreen(navController, osintViewModel) }
             composable("usb_inspector") { UsbInspectorScreen(navController, usbInspectorViewModel) }
             composable("usb_hid_lab") { BadUsbScreen(navController, badUsbViewModel, usbInspectorViewModel) }
             composable("ir") { InfraredScreen(navController) }
